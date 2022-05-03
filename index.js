ef1ee21bfd2fe18f47a15e3b4233e6c898a0ccd1
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-05-03 14:20:11 +0300

    final touches

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const darkModeToggle = document.getElementById("dark-mode-toggle");
+const themeToggle = document.getElementById("theme-toggle");
+function enableDarkMode() {
+  document.body.classList.remove("light-theme");
+  document.body.classList.add("dark-theme");
+  themeToggle.setAttribute("aria-label", "Switch to light theme");
+}
 
-darkModeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-  console.log("test");
+function enableLightMode() {
+  document.body.classList.remove("dark-theme");
+  document.body.classList.add("light-theme");
+  themeToggle.setAttribute("aria-label", "Switch to dark theme");
+}
+
+function setThemePreference() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    enableDarkMode();
+    return;
+  }
+  enableLightMode();
+}
+
+document.onload = setThemePreference();
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.contains("dark-theme")
+    ? enableLightMode()
+    : enableDarkMode();
 });
